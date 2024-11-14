6f0d50526b2e0a6051427313b5a33b5f97fc2900
Lorenzo <125370297+lorenzopugina@users.noreply.github.com>
2024-11-14 17:08:53 -0300

    Refatorado

--- funcoes.js
+++ funcoes.js
@@ -1,37 +1,38 @@
 const canvas = document.getElementById("meuCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
+const larguraCanvas = canvas.clientWidth;
+const alturaCanvas = canvas.clientHeight;
+canvas.width = larguraCanvas;
+canvas.height = alturaCanvas;
 
 const dino = new Image();
 const cacto = new Image();
 dino.src = "dino.png";
 cacto.src = "cacto.png";
 
-// Variaveis pro dinossauro
-let posY = canvas.height - 65; 
+// Variaveis para o dinossauro
+let posY = alturaCanvas - 65; 
 let pulando = false; 
 let alturaPulo = 0; 
 let gravidade = 0.15; 
 
-//Variaveis pro cacto
-let tamanho = canvas.width;
-let posX = [tamanho, tamanho, tamanho, tamanho, tamanho]; 
+//Variaveis para o cacto
+let posX = [larguraCanvas, larguraCanvas, larguraCanvas, larguraCanvas, larguraCanvas]; 
 let cactos = [0, 0, 0, 0, 0]; 
 let quantCactos = 0;
 let velocidade = 4;
 
-//Variaveis pro jogo
+//Variaveis para o jogo
 let pontuacao = 0;
 let gameOver = false;
 
 //Se a tecla foi apertada
 document.addEventListener("keydown", function(event) {
-    if (event.key === "ArrowUp" && !gameOver) {
+    if (event.key === "ArrowUp") {
         pular();
     }
-    if(event.key === "ArrowDown" && !gameOver){
+    if(event.key === "ArrowDown"){
         gravidade = 0.65;
     }
 });
@@ -44,7 +45,7 @@ document.addEventListener("keyup", function(event) {
 });
 
 function pular() {
-    if (!pulando && !gameOver) {
+    if (!pulando) {
         pulando = true;
         alturaPulo = 5;
     }
@@ -55,7 +56,7 @@ function verificarColisao() {
     for (let i = 0; i < 5; i++) {
         
         if (cactos[i] === 1) {
-            if (posX[i] < 50 && posX[i] > 20 && posY + 60 >= canvas.height - 50){
+            if (posX[i] > 0 && posX[i] < 50 && posY + 45 >= alturaCanvas - 50){
                 gameOver = true; 
             }
         }
@@ -64,30 +65,31 @@ function verificarColisao() {
 
 function atualizar() {
     if (gameOver) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, larguraCanvas, alturaCanvas);
         ctx.font = "40px Arial";
-        ctx.fillText("Fim de jogo", canvas.width / 2 - 150, canvas.height / 2);
-        ctx.fillText("Pontuação Final: " + pontuacao, canvas.width / 2 - 180, canvas.height / 2 + 50);
+        ctx.fillText("Fim de jogo", larguraCanvas / 2 - 150, alturaCanvas / 2);
+        ctx.fillText("Pontuação Final: " + pontuacao, larguraCanvas / 2 - 180, alturaCanvas / 2 + 50);
         return;
     }
 
     // Limpa o canvas antes de redesenhar
-    ctx.clearRect(0, 0, canvas.width, canvas.height-10);
-
+    ctx.clearRect(0, 0, larguraCanvas, alturaCanvas - 10);
+    
     if (pulando) {
         posY -= alturaPulo; // negativo para ir para cima
         alturaPulo -= gravidade; 
 
         // Verifica se o dinossauro atingiu o chão
-        if (posY >= canvas.height - 65) {
-            posY = canvas.height - 65; // Reseta a posição 
+        if (posY >= alturaCanvas - 65) {
+            posY = alturaCanvas - 65; // Reseta a posição 
             pulando = false; 
         }
     }
-    // Redesenha o dinossauro
+
+    // Desenha o dinossauro
     ctx.drawImage(dino, 20, posY, 50, 60);
 
-    // Cria cactos "Aleatoriamente"
+    // Cria cactos aleatoriamente
     if (quantCactos < 5) {
         for (let i = 0; i < 5; i++) {
             if (cactos[i] === 0 && gerarNumeroAleatorio(0, 120) === 10) { 
@@ -104,34 +106,35 @@ function atualizar() {
             posX[i] -= velocidade; 
 
             // Se o cacto sair da tela
-            if (posX[i] < -100) {
-                posX[i] = tamanho; 
+            if (posX[i] < -40) {
+                posX[i] = larguraCanvas; 
                 cactos[i] = 0; 
                 quantCactos--; 
                 pontuacao++;
             }
 
             // Desenha o cacto
-            ctx.drawImage(cacto, posX[i], canvas.height - 50, 40, 40);
+            ctx.drawImage(cacto, posX[i], alturaCanvas - 50, 40, 40);
         }
     }
 
     // Desenha a pontuação
     ctx.font = "15px Arial";
-    ctx.fillText("Pontuação: " + pontuacao, canvas.width - 100, 20); 
+    ctx.fillText("Pontuação: " + pontuacao, larguraCanvas - 100, 20); 
 
     verificarColisao();
 }
 
-// executa a função atualizar 60x por sec
-setInterval(atualizar, 1000 / 60);
-
 // Desenha o chão
 ctx.beginPath();
-ctx.moveTo(0, canvas.height - 10); // Início da linha
-ctx.lineTo(canvas.width, canvas.height - 10); // Fim da linha
+ctx.moveTo(0, alturaCanvas - 10); // Início da linha
+ctx.lineTo(larguraCanvas, alturaCanvas - 10); // Fim da linha
 ctx.stroke();
+
+// executa a função atualizar 60x por sec
+setInterval(atualizar, 1000 / 60);
 
 function gerarNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
